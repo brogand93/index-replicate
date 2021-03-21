43c1f06f1003650016b832ff876a4c5aca55de06
brogand93 <brogand93@gmail.com>
2021-03-21 20:20:08 +0000

    Add tests

--- internal/replicator/replicate.go
+++ internal/replicator/replicate.go
@@ -16,8 +16,9 @@ const (
 	outputCSV   = "index_info.csv"
 )
 
-func (client *Client) Run(outputToCsv bool) error {
-	index, err := index.Get(client.Index)
+func (c *Client) Run(outputToCsv bool) error {
+	indexClient := index.NewClient()
+	index, err := indexClient.Get(c.Index)
 	if err != nil {
 		return err
 	}
@@ -27,11 +28,11 @@ func (client *Client) Run(outputToCsv bool) error {
 	replicatedIndex := ReplicatedIndex{}
 
 	for _, component := range index.Components {
-		if (percentageCovered + component.Weight) < client.Percentage {
-			weightInReplicatedIndex := client.componentWeight(component)
-			sharesInRerplicatedIndex := client.sharesToBuy(component, weightInReplicatedIndex)
+		if (percentageCovered + component.Weight) < c.Percentage {
+			weightInReplicatedIndex := c.componentWeight(component)
+			sharesInRerplicatedIndex := c.sharesToBuy(component, weightInReplicatedIndex)
 			valueInIndex := float32(component.Price) * sharesInRerplicatedIndex
-			if client.RoundShareQuantity {
+			if c.RoundShareQuantity {
 				sharesInRerplicatedIndex = float32(
 					math.Round(float64(sharesInRerplicatedIndex)),
 				)
@@ -51,7 +52,7 @@ func (client *Client) Run(outputToCsv bool) error {
 
 	replicatedIndex.Total = totalContribution
 
-	err = client.write(replicatedIndex, outputToCsv)
+	err = c.write(replicatedIndex, outputToCsv)
 	if err != nil {
 		log.Println(err.Error())
 		return err
@@ -62,23 +63,23 @@ func (client *Client) Run(outputToCsv bool) error {
 
 // componentWeight calculates the weight of an individual component in
 // the replciated index
-func (client *Client) componentWeight(component index.Component) float32 {
-	if component.Price > client.Contribution {
+func (c *Client) componentWeight(component index.Component) float32 {
+	if component.Price > c.Contribution {
 		return 0
 	}
 
-	return component.Weight * (100 / client.Percentage)
+	return component.Weight * (100 / c.Percentage)
 }
 
 // sharesToBuy calculates the number of component shares to buy for an individual
 // company in the replciated index
-func (client *Client) sharesToBuy(component index.Component, weight float32) float32 {
+func (c *Client) sharesToBuy(component index.Component, weight float32) float32 {
 	// Compute the portion of contribution that should be used on this component
-	availableForComponent := float32(client.Contribution) / 100 * weight
+	availableForComponent := float32(c.Contribution) / 100 * weight
 	return availableForComponent / float32(component.Price)
 }
 
-func (client *Client) write(replicatedIndex ReplicatedIndex, outputToCsv bool) error {
+func (c *Client) write(replicatedIndex ReplicatedIndex, outputToCsv bool) error {
 	data := [][]string{}
 
 	for _, component := range replicatedIndex.Components {
@@ -91,19 +92,19 @@ func (client *Client) write(replicatedIndex ReplicatedIndex, outputToCsv bool) e
 		})
 	}
 
-	err := client.writeTable(data, replicatedIndex.Total)
+	err := c.writeTable(data, replicatedIndex.Total)
 	if err != nil {
 		return err
 	}
 
 	if outputToCsv {
-		return client.writeCSV(data)
+		return c.writeCSV(data)
 	}
 
 	return nil
 }
 
-func (client *Client) writeTable(data [][]string, total float32) error {
+func (c *Client) writeTable(data [][]string, total float32) error {
 	table := tablewriter.NewWriter(os.Stdout)
 
 	table.SetHeader([]string{
@@ -119,7 +120,7 @@ func (client *Client) writeTable(data [][]string, total float32) error {
 
 }
 
-func (client *Client) writeCSV(data [][]string) error {
+func (c *Client) writeCSV(data [][]string) error {
 	file, err := os.Create(outputCSV)
 	if err != nil {
 		return err
--- pkg/index/index_test.go
+++ pkg/index/index_test.go
@@ -1,1 +1,104 @@
 package index_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/brogand93/index-replicate/pkg/index"
+)
+
+type HttpMock struct {
+	res http.Response
+}
+
+func (c *HttpMock) Do(req *http.Request) (*http.Response, error) {
+	return &c.res, nil
+}
+
+func Test_Get(t *testing.T) {
+	type args struct {
+		index string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+		mockRes http.Response
+		want    index.Index
+	}{
+		{
+			name: "Simple index request should be successful",
+			args: args{
+				index: "sample-index",
+			},
+			wantErr: nil,
+			mockRes: http.Response{
+				StatusCode: 200,
+				Body: ioutil.NopCloser(bytes.NewReader([]byte(
+					`{"companyList":[{"symbol":"SI","name":"sample-index","weight":100,"rank":1,"changePercent":0.0250020835069531,"lastPrice":100,"netChange":0.03,"changeColor":"green"}]}`,
+				))),
+			},
+			want: index.Index{
+				Components: []index.Component{
+					{
+						Name:   "sample-index",
+						Price:  100.0,
+						Rank:   1,
+						Symbol: "SI",
+						Weight: 100.0,
+						Value:  0,
+					},
+				},
+			},
+		}, {
+			name: "invalid index returns NotFound error",
+			args: args{
+				index: "invalid-index",
+			},
+			wantErr: index.NotFound{
+				Message: "invalid-index is not a valid index for this data source",
+			},
+			mockRes: http.Response{
+				StatusCode: 404,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(""))),
+			},
+			want: index.Index{},
+		}, {
+			name: "index data source error returns DataSourceUnavailable error",
+			args: args{
+				index: "error-index",
+			},
+			wantErr: index.DataSourceUnavailable{
+				Message: "https://svcga.com/sc/index data source is either unavailable or misconfigured",
+			},
+			mockRes: http.Response{
+				StatusCode: 500,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(""))),
+			},
+			want: index.Index{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockClient := index.Client{
+				Http: &HttpMock{
+					res: tt.mockRes,
+				},
+			}
+			got, err := mockClient.Get(tt.args.index)
+
+			if err != tt.wantErr {
+				t.Errorf("Get() = %v, want %v", err, tt.wantErr)
+			}
+
+			if !reflect.DeepEqual(got, &tt.want) {
+				t.Errorf("Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
